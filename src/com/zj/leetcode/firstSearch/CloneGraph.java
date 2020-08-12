05f913a8876f797292d4cb9113710cbad0ac9c45
jianzhang <jianzhang@yangqianguan.com>
2020-08-12 11:36:08 +0800

    +)克隆图

---
+++ src/com/zj/leetcode/firstSearch/CloneGraph.java
@@ -0,0 +1,103 @@
+package com.zj.leetcode.firstSearch;
+
+import java.util.*;
+
+/**
+ * 给你无向 连通 图中一个节点的引用，请你返回该图的 深拷贝（克隆）。
+ *
+ * 图中的每个节点都包含它的值 val（int） 和其邻居的列表（list[Node]）。
+ *
+ * class Node {
+ *     public int val;
+ *     public List<Node> neighbors;
+ * }
+ *  
+ *
+ * 测试用例格式：
+ *
+ * 简单起见，每个节点的值都和它的索引相同。例如，第一个节点值为 1（val = 1），第二个节点值为 2（val = 2），以此类推。该图在测试用例中使用邻接列表表示。
+ *
+ * 邻接列表 是用于表示有限图的无序列表的集合。每个列表都描述了图中节点的邻居集。
+ *
+ * 给定节点将始终是图中的第一个节点（值为 1）。你必须将 给定节点的拷贝 作为对克隆图的引用返回。
+ *
+ * 提示：
+ *
+ * 节点数不超过 100 。
+ * 每个节点值 Node.val 都是唯一的，1 <= Node.val <= 100。
+ * 无向图是一个简单图，这意味着图中没有重复的边，也没有自环。
+ * 由于图是无向的，如果节点 p 是节点 q 的邻居，那么节点 q 也必须是节点 p 的邻居。
+ * 图是连通图，你可以从给定节点访问到所有节点。
+ *
+ */
+public class CloneGraph {
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> visited = new HashMap<>();
+        return cloneGraphBfs(node, visited);
+    }
+
+    public Node cloneGraphDfs(Node node, Map<Node, Node> visited) {
+        if (node == null) return node;
+
+        // 如果该节点已经被访问过了，则直接从哈希表中取出对应的克隆节点返回
+        if (visited.containsKey(node)) return visited.get(node);
+
+        // 克隆节点，注意到为了深拷贝我们不会克隆它的邻居的列表
+        Node cloneNode = new Node(node.val, new ArrayList<>());
+        visited.put(node, cloneNode);
+
+        // 遍历该节点的邻居并更新克隆节点的邻居列表
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraphDfs(neighbor, visited));
+        }
+        return cloneNode;
+    }
+
+    public Node cloneGraphBfs(Node node, Map<Node, Node> visited) {
+        if (node == null) return node;
+
+        // 将题目给定的节点添加到队列
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(node);
+        // 克隆第一个节点并存储到哈希表中
+        visited.put(node, new Node(node.val, new ArrayList<>()));
+
+        while (!queue.isEmpty()) {
+            // 取出队列的头节点
+            Node curNode = queue.remove();
+            // 遍历该节点的邻居
+            for (Node neighbor : curNode.neighbors) {
+                // 如果没有被访问过，就克隆并存储在哈希表中
+                if (!visited.containsKey(neighbor)) {
+                    visited.put(neighbor, new Node(neighbor.val, new ArrayList<>()));
+                    // 将邻居节点加入队列中
+                    queue.add(neighbor);
+                }
+                // 更新当前节点的邻居列表
+                visited.get(curNode).neighbors.add(visited.get(neighbor));
+            }
+        }
+        return visited.get(node);
+    }
+}
