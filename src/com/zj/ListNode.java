5adc2b4304f7e7aeb3563e50744c6f340780e997
jianzhang <jianzhang@yangqianguan.com>
2020-10-13 11:07:29 +0800

    +) 两两交换链表中的节点

--- src/com/zj/ListNode.java
+++ src/com/zj/ListNode.java
@@ -3,7 +3,15 @@ package com.zj;
 public class ListNode {
     public int val;
     public ListNode next = null;
+
+    public ListNode() {}
+
     public ListNode(int val) {
         this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
