1fbc065c7915fa42dadd2f1d742823ba0a9ba88c
jianzhang <jianzhang@yangqianguan.com>
2021-03-19 10:23:57 +0800

    +) 设计停车系统

---
+++ src/com/zj/leetcode/design/ParkingSystem.java
@@ -0,0 +1,29 @@
+package com.zj.leetcode.design;
+
+/**
+ * Created on 3/19/21.
+ * 请你给一个停车场设计一个停车系统。停车场总共有三种不同大小的车位：大，中和小，每种尺寸分别有固定数目的车位。
+ *
+ * 请你实现 ParkingSystem 类：
+ *
+ * ParkingSystem(int big, int medium, int small) 初始化 ParkingSystem 类，三个参数分别对应每种停车位的数目。
+ * bool addCar(int carType) 检查是否有 carType 对应的停车位。 carType 有三种类型：大，中，小，分别用数字 1， 2 和 3 表示。一辆车只能停在  carType 对应尺寸的停车位中。如果没有空车位，请返回 false ，否则将该车停入车位并返回 true 。
+ *
+ * 链接：https://leetcode-cn.com/problems/design-parking-system
+ */
+
+public class ParkingSystem {
+  private int[] parkCounter;
+
+  public ParkingSystem(int big, int medium, int small) {
+    parkCounter = new int[] {big, medium, small};
+  }
+
+  public boolean addCar(int carType) {
+    if (parkCounter[carType - 1] > 0) {
+      parkCounter[carType - 1]--;
+      return true;
+    }
+    return false;
+  }
+}
