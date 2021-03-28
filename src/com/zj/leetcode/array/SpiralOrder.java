4055d93d66a4bd08dcfbf89b7b5b6ca974e952b0
suyanruo <dimghost1@gmail.com>
2021-03-28 17:47:54 +0800

    *) 顺时针螺旋顺序遍历二维矩阵

--- src/com/zj/leetcode/array/SpiralOrder.java
+++ src/com/zj/leetcode/array/SpiralOrder.java
@@ -8,58 +8,103 @@ import java.util.List;
  * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
  *  
  * 示例 1：
- *
+ * <p>
  * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
  * 输出：[1,2,3,6,9,8,7,4,5]
- *
+ * <p>
  * 链接：https://leetcode-cn.com/problems/spiral-matrix
  */
 
 public class SpiralOrder {
-  public List<Integer> spiralOrder(int[][] matrix) {
-    List<Integer> result = new ArrayList<>();
-    int x = 0, y = 0;
-    int row = matrix.length;
-    int colon = matrix[0].length;
-    int times = Math.min(row % 2 == 0 ? row / 2 : row / 2 + 1,
-        colon % 2 == 0 ? colon / 2 : colon / 2 + 1);
-    for (int i = 0; i < times; i++) {
-      while (y < colon - i) {
-        result.add(matrix[x][y++]);
-        if (y == colon - i) {
-          y--;
-          x++;
-          break;
+    public class Solution {
+        List<Integer> result = new ArrayList<>();
+
+        public List<Integer> spiralOrder(int[][] matrix) {
+            traverse(matrix, 0, 0, matrix.length - 1, matrix[0].length - 1);
+            return result;
         }
-      }
-      while (x < row - i) {
-        result.add(matrix[x++][y]);
-        if (x == row - i) {
-          x--;
-          y--;
-          break;
+
+        private void traverse(int[][] matrix, int startX, int startY, int endX, int endY) {
+            if (startX > endX || startY > endY) {
+                return;
+            }
+            if (startX == endX) {
+                for (int i = startY; i <= endY; i++) {
+                    result.add(matrix[startX][i]);
+                }
+                return;
+            }
+            if (startY == endY) {
+                for (int i = startX; i <= endX; i++) {
+                    result.add(matrix[i][endY]);
+                }
+                return;
+            }
+            for (int i = startY; i < endY; i++) {
+                result.add(matrix[startX][i]);
+            }
+            for (int i = startX; i < endX; i++) {
+                System.out.println("x:" + i + ", y:" + endY);
+                result.add(matrix[i][endY]);
+            }
+            for (int i = endY; i > startY; i--) {
+                result.add(matrix[endX][i]);
+            }
+            for (int i = endX; i > startX; i--) {
+                result.add(matrix[i][startY]);
+            }
+            traverse(matrix, startX + 1, startY + 1, endX - 1, endY - 1);
         }
-      }
-      if (i == times - 1 && Math.min(row, colon) % 2 == 1) {
-        break;
-      }
-      while (y >= i) {
-        result.add(matrix[x][y--]);
-        if (y < i) {
-          y++;
-          x--;
-          break;
-        }
-      }
-      while (x > i) {
-        result.add(matrix[x--][y]);
-        if (x == i) {
-          x++;
-          y++;
-          break;
-        }
-      }
     }
-    return result;
-  }
+
+    public class Solution2 {
+        public List<Integer> spiralOrder(int[][] matrix) {
+            List<Integer> result = new ArrayList<>();
+            int x = 0, y = 0;
+            int row = matrix.length;
+            int colon = matrix[0].length;
+            int times = Math.min(row % 2 == 0 ? row / 2 : row / 2 + 1,
+                    colon % 2 == 0 ? colon / 2 : colon / 2 + 1);
+            for (int i = 0; i < times; i++) {
+                while (y < colon - i) {
+                    result.add(matrix[x][y++]);
+                    if (y == colon - i) {
+                        y--;
+                        x++;
+                        break;
+                    }
+                }
+                while (x < row - i) {
+                    result.add(matrix[x++][y]);
+                    if (x == row - i) {
+                        x--;
+                        y--;
+                        break;
+                    }
+                }
+                if (i == times - 1 && Math.min(row, colon) % 2 == 1) {
+                    break;
+                }
+                while (y >= i) {
+                    result.add(matrix[x][y--]);
+                    if (y < i) {
+                        y++;
+                        x--;
+                        break;
+                    }
+                }
+                while (x > i) {
+                    result.add(matrix[x--][y]);
+                    if (x == i) {
+                        x++;
+                        y++;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+    }
+
 }
--- src/com/zj/leetcode/string/StringMultiple.java
+++ src/com/zj/leetcode/string/StringMultiple.java
@@ -1,4 +1,4 @@
-package com.zj.leetcode.array;
+package com.zj.leetcode.string;
 
 /**
  * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
@@ -17,6 +17,8 @@ package com.zj.leetcode.array;
  * num1 和 num2 只包含数字 0-9。
  * num1 和 num2 均不以零开头，除非是数字 0 本身。
  * 不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。
+ *
+ * 链接：https://leetcode-cn.com/problems/multiply-strings/
  *
  */
 public class StringMultiple {
