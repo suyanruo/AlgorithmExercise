4fa8329b338bba969e1061e288ba78dd5140d6e2
suyanruo <dimghost1@gmail.com>
2021-03-28 14:57:46 +0800

    +) 二叉搜索树迭代器

---
+++ src/com/zj/leetcode/tree/BSTIterator.java
@@ -0,0 +1,52 @@
+package com.zj.leetcode.tree;
+
+import com.zj.model.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+/**
+ * Created by zhangjian on 2021/3/28.
+ * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+ *BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+ * boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+ * int next()将指针向右移动，然后返回指针处的数字。
+ * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+ *
+ * 链接：https://leetcode-cn.com/problems/binary-search-tree-iterator
+ */
+public class BSTIterator {
+    private Deque<Integer> treeValue;
+
+    public BSTIterator(TreeNode root) {
+        treeValue = new ArrayDeque<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                treeValue.add(cur.val);
+                cur = cur.right;
+            }
+        }
+    }
+
+    public int next() {
+        if (treeValue != null && !treeValue.isEmpty()) {
+            return treeValue.pop();
+        }
+        return -1;
+    }
+
+    public boolean hasNext() {
+        if (treeValue == null) {
+            return false;
+        }
+        return !treeValue.isEmpty();
+    }
+}
