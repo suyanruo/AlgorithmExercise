71c9ae5065816227162e1bb937813294ac9b6b8d
jianzhang <jianzhang@yangqianguan.com>
2021-04-19 11:04:47 +0800

    +) 移除元素

---
+++ src/com/zj/leetcode/doublePointer/RemoveElement.java
@@ -0,0 +1,25 @@
+package com.zj.leetcode.doublePointer;
+
+/**
+ * Created on 4/19/21.
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ *
+ * 链接：https://leetcode-cn.com/problems/remove-element
+ */
+
+public class RemoveElement {
+  public int removeElement(int[] nums, int val) {
+    int end = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != val) {
+        if (end != i) {
+          nums[end] = nums[i];
+        }
+        end++;
+      }
+    }
+    return end;
+  }
+}
