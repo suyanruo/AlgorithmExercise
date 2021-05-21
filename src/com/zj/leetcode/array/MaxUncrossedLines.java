c8e5bf8e7d921bed1a89da6e8a4219bc724d0554
jianzhang <jianzhang@yangqianguan.com>
2021-05-21 11:35:51 +0800

    +) 不相交的线

---
+++ src/com/zj/leetcode/array/MaxUncrossedLines.java
@@ -0,0 +1,36 @@
+package com.zj.leetcode.array;
+
+/**
+ * Created by ZhangJian on 2021/5/21.
+ * 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+ * 现在，可以绘制一些连接两个数字 nums1[i]和 nums2[j]的直线，这些直线需要同时满足满足：
+ * nums1[i] == nums2[j]
+ * 且绘制的直线不与任何其他连线（非水平线）相交。
+ * 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+ * 以这种方法绘制线条，并返回可以绘制的最大连线数。
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,4,2], nums2 = [1,2,4]
+ * 输出：2
+ * 解释：可以画出两条不交叉的线，如上图所示。 
+ * 但无法画出第三条不相交的直线，因为从 nums1[1]=4 到 nums2[2]=4 的直线将与从 nums1[2]=2 到 nums2[1]=2 的直线相交。
+ *
+ * 链接：https://leetcode-cn.com/problems/uncrossed-lines
+ * ref: https://leetcode-cn.com/problems/uncrossed-lines/solution/gong-shui-san-xie-noxiang-xin-ke-xue-xi-bkaas/
+ */
+public class MaxUncrossedLines {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[][] f = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]);
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    f[i][j] = Math.max(f[i][j], f[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return f[m][n];
+    }
+}
