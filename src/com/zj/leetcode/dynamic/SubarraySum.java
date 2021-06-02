752cff929aa2cf0775d255c39636ad30480dddf6
jianzhang <jianzhang@yangqianguan.com>
2021-06-02 14:45:19 +0800

    +) 连续的子数组和

---
+++ src/com/zj/leetcode/dynamic/SubarraySum.java
@@ -0,0 +1,39 @@
+package com.zj.leetcode.dynamic;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by ZhangJian on 2021/6/2.
+ * 给你一个整数数组 nums 和一个整数k ，编写一个函数来判断该数组是否含有同时满足下述条件的连续子数组：
+ * 子数组大小 至少为 2 ，且
+ * 子数组元素总和为 k 的倍数。
+ * 如果存在，返回 true ；否则，返回 false 。
+ * 如果存在一个整数 n ，令整数 x 符合 x = n * k ，则称 x 是 k 的一个倍数。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [23,2,4,6,7], k = 6
+ * 输出：true
+ * 解释：[2,4] 是一个大小为 2 的子数组，并且和为 6 。
+ *
+ * 链接：https://leetcode-cn.com/problems/continuous-subarray-sum
+ * ref: https://leetcode-cn.com/problems/continuous-subarray-sum/solution/gong-shui-san-xie-tuo-zhan-wei-qiu-fang-1juse/
+ */
+public class SubarraySum {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int[] sum = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 2; i <= n; i++) {
+            set.add(sum[i - 2] % k);
+            if (set.contains(sum[i] % k)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
