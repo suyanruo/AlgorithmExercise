e1bbfe3a56d4b6fe5b3b1dca60f689c49e20c683
jianzhang <jianzhang@yangqianguan.com>
2021-07-02 14:30:17 +0800

    +) 盛最多水的容器

---
+++ src/com/zj/leetcode/greedyAlgorithm/MaxArea.java
@@ -0,0 +1,29 @@
+package com.zj.leetcode.greedyAlgorithm;
+
+/**
+ * Created by ZhangJian on 2021/7/2.
+ * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 说明：你不能倾斜容器。
+ *
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ *
+ * 链接：https://leetcode-cn.com/problems/container-with-most-water
+ * ref: https://leetcode-cn.com/problems/container-with-most-water/solution/shua-chuan-lc-shuang-zhi-zhen-tan-xin-ji-52gf/
+ */
+public class MaxArea {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int ans = 0;
+        while (left < right) {
+            ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    }
+}
