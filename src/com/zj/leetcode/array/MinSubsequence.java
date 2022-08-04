651369f7d8491fd16d3a14cad364a140351d9bee
zhangjian <dimghost1@gmail.com>
2022-08-04 13:53:28 +0800

    +) 非递增顺序的最小子序列

---
+++ src/com/zj/leetcode/array/MinSubsequence.java
@@ -0,0 +1,32 @@
+package com.zj.leetcode.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: zhangjian
+ * @date: 2022/8/4 13:47
+ * @description: 非递增顺序的最小子序列
+ * 给你一个数组 nums，请你从中抽取一个子序列，满足该子序列的元素之和 严格 大于未包含在该子序列中的各元素之和。
+ * 如果存在多个解决方案，只需返回 长度最小 的子序列。如果仍然有多个解决方案，则返回 元素之和最大 的子序列。
+ * 与子数组不同的地方在于，「数组的子序列」不强调元素在原数组中的连续性，也就是说，它可以通过从数组中分离一些（也可能不分离）元素得到。
+ * 注意，题目数据保证满足所有约束条件的解决方案是 唯一 的。同时，返回的答案应当按 非递增顺序 排列。
+ *
+ * 链接：https://leetcode.cn/problems/minimum-subsequence-in-non-increasing-order
+ * ref: https://leetcode.cn/problems/minimum-subsequence-in-non-increasing-order/solution/by-ac_oier-766t/
+ */
+public class MinSubsequence {
+    public List<Integer> minSubsequence(int[] nums) {
+        int idx = nums.length - 1, sum = 0, child = 0;
+        Arrays.sort(nums);
+        for (int i : nums) sum += i;
+        List<Integer> ans = new ArrayList<>();
+        while (child <= sum) {
+            child += nums[idx];
+            sum -= nums[idx];
+            ans.add(nums[idx--]);
+        }
+        return ans;
+    }
+}
